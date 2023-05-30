955b5165a961ae757fd14d6c3990b4bb50f5c3d1
JoaoPaulo-creator <joaosarto@gmail.com>
2023-05-29 22:43:38 -0300

    refactor: Renderizando mensagem lista de repos for vazia

--- src/app/users/git/page.tsx
+++ src/app/users/git/page.tsx
@@ -1,34 +1,29 @@
 'use client'
 
-export default async function GitInfo() {
+interface RepoProps {
+  id: number,
+  name: string,
+  url: string
+}
 
-  const request = await fetch('http://localhost:3332/prices');
-  const data = await request.json();
 
-  const { name, code, bid } = data.USDBRL
+
+export default async function GitRepos() {
+  const response = await fetch('http://localhost:3333/git')
+  const data: RepoProps[] = await response.json()
+
+
+
+
+
   return (
     <div>
+      {data.length !== 0 ? data.map((repo: RepoProps) => (
+        <div key={repo.id} >
+          <p className="px-2 h-8 mb-4 flex items-center justify-start rounded-md text-white bg-slate-800"><a href={repo.url}>{repo.name}</a></p>
+        </div>
+      )) : <p>Nenhum repositório encontrado</p>}
+    </div >
+  )
 
-      {/*
-      justify move o elemento no eixo X, enquanto que items no eixo Y
-
-      No css puro seria align-items e justify-content (acredito eu)
-      */}
-      <div
-        className="h-16 w-32 bg-red-300 text-black text-center flex items-center justify-center">
-        TESTE
-      </div>
-
-
-      <p>{name}</p>
-      <p className="text-2xl text-gray-900 mt-2">
-
-        {/* Se eu quiser passar o objeto inteiro que vem do back, para ser renderizado no front
-        devo usar o JSON.stringify.
-        Passando as chaves desestruturadas, funciona sem precisar usar o JSON.stringify.
-         */}
-        {code} R$ {bid}
-      </p>
-    </div>
-  );
 }
--- src/app/users/list/page.tsx
+++ src/app/users/list/page.tsx
@@ -10,21 +10,22 @@ interface UserProps {
 
 export default async function UsersList() {
 
-  const response = await fetch('http://localhost:3002/users')
-  const data: UserProps[] = await response.json()
+  const response = await fetch('http://localhost:3333/users')
+  const data = await response.json()
 
 
   return (
     <div>
-      <a href="/" className="text-blue-500">Voltar para home</a>
+      <a href="/" className="text-blue-500">Voltar para home</a> <br />
       <a href="/users/new-contact" className="text-gray-500 inline-block self-end leading-none">Add contato</a>
-      {data.map((user) => (
-        <div className="mt-4 bg-gray-100 p-4" key={user.id}>
-          Nome: {user.name} <br />
-          Idade: {user.age} <br />
-          Email: {user.email} <br />
+      {data.map((user: UserProps) => (
+        <div key={user.id} className="mt-4 bg-gray-100 p-4" >
+          <p>Nome: {user.name}</p>
+          <p>Idade: {user.age}</p>
+          <p>E-mail: {user.email}</p>
         </div>
-      ))}
-    </div>
+      ))
+      }
+    </div >
   )
 }
