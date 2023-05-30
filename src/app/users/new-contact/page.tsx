589ba59d3b4075e4ebeed5c64f5148374068d971
JoaoPaulo-creator <joaosarto@gmail.com>
2023-05-29 23:12:19 -0300

    feat: Chamando endpoint para salvar funcionario

--- src/app/users/new-contact/page.tsx
+++ src/app/users/new-contact/page.tsx
@@ -3,21 +3,76 @@
 import { useState } from "react"
 
 export default function NewContact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    salary: ''
+  });
 
-  const [firstName, setFirstName] = useState<string>('')
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send form data to the backend using Fetch API
+    fetch('https://dummy.restapiexample.com/api/v1/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from the backend
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle any errors
+        console.error(error);
+      });
+  };
 
   return (
-    <div>
-      <a href="/users/list" className="text-gray-900 bg-slate-200">Contatos</a>
-      <h1>NewContact</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="age">Age:</label>
+        <input
+          type="age"
+          id="age"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="salary">Salary:</label>
+        <input
+          type="salary"
+          id="salary"
+          name="salary"
+          value={formData.salary}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" className="rounded-lg bg-blue-500 py-2 px-4 text-gray-100">Submit</button>
+    </form>
+  );
 
-      <label htmlFor="first-name">
-        First name:
-        <input className="rounded border border-gray-300 ml-2" value={firstName} type="text" onChange={(event) => setFirstName(event.target.value)} placeholder="First name" />
-      </label>
-      {firstName !== '' && <p>Your name is {firstName}!</p>}
 
-    </div>
-  )
 }
